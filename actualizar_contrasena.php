8f5e206e09ee84842cd5de1a6e1b33bcc2947665
Brahian Rendon <brahian_rendon82231@elpoli.edu.co>
2025-03-28 18:33:24 -0500

    estos es una segunda prueba brahian

--- actualizar_contrasena.php
+++ actualizar_contrasena.php
@@ -64,7 +64,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <script>
             Swal.fire('Éxito', '<?php echo $mensaje_exito; ?>', 'success')
 //esto es una prueba de un cambio
-//de contraseñajdcdpjdn
+
             .then(() => window.location.href = 'index.php');
         </script>
     <?php endif; ?>
