84b8160e58a3e91f52f41df4563991bcce898e51
AndresSanchez12323 <edwin_sanchez82231@elpoli.edu.co>
2025-04-02 21:44:30 -0500

    Base de Datos Final

--- vial_servi.sql
+++ vial_servi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 02-04-2025 a las 06:51:48
+-- Tiempo de generación: 03-04-2025 a las 04:43:06
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.0.30
 
@@ -76,6 +76,7 @@ CREATE TABLE `empleados` (
   `Contraseña` varchar(100) NOT NULL,
   `Rol_id` int(11) NOT NULL,
   `Email` varchar(100) DEFAULT NULL,
+  `Telefono` varchar(20) NOT NULL,
   `intentos_fallidos` int(11) DEFAULT 0,
   `fecha_hora_bloqueo` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -84,27 +85,13 @@ CREATE TABLE `empleados` (
 -- Volcado de datos para la tabla `empleados`
 --
 
-INSERT INTO `empleados` (`Cedula_Empleado_id`, `Nombre`, `Apellido`, `Contraseña`, `Rol_id`, `Email`, `intentos_fallidos`, `fecha_hora_bloqueo`) VALUES
-(1010, 'brian', 'rendon', '$2y$10$JQSVWMvzAwNhiA0JYD0fM.uUxZzylPV2Xx/7fp214BmOwTtSRSs1.', 2, NULL, 0, NULL),
-(1040, 'edwin', 'sanchez', '$2y$10$dGu8U8jqn6E1BAhIqHk3Fe484U83cG7c/PfCpYi5mfxia5R17tTw6', 1, NULL, 0, NULL),
-(1234, 'nube', 'sanchez', '$2y$10$/dIASvz2jJF8wZDQs4EmUuMlMlgu72rhFfnGkfEXj5xLdMixvQ9s.', 5, 'nube9916@gmail.com', 0, NULL),
-(2025, 'patricia', 'patiño', '$2y$10$aiUAA6oEMZVf.YpYhBRwUOOBgbpFV0fm8f6o5.zQQiiqkDA/RcMzK', 3, NULL, 0, NULL),
-(4040, 'Super', 'Admin', '$2y$10$F22tgMXGlfLCFN2dRocsWOOlfWflEXeIbDcCJtfJXsEwMhnpRLtmS', 0, NULL, 0, NULL);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `geolocalización`
---
-
-CREATE TABLE `geolocalización` (
-  `Geolocalización_id` int(11) NOT NULL,
-  `Servicios_realizados_id_Geolocalización` int(11) DEFAULT NULL,
-  `Ubicación_Recogida` varchar(255) DEFAULT NULL,
-  `Ubicación_Entrega` varchar(255) DEFAULT NULL,
-  `Fecha_Recogida` date DEFAULT NULL,
-  `Fecha_Entrega` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+INSERT INTO `empleados` (`Cedula_Empleado_id`, `Nombre`, `Apellido`, `Contraseña`, `Rol_id`, `Email`, `Telefono`, `intentos_fallidos`, `fecha_hora_bloqueo`) VALUES
+(1010, 'brian', 'rendon', '$2y$10$JQSVWMvzAwNhiA0JYD0fM.uUxZzylPV2Xx/7fp214BmOwTtSRSs1.', 2, 'brianloquito@gmail.com', '', 0, NULL),
+(1040, 'edwin', 'sanchez', '$2y$10$dGu8U8jqn6E1BAhIqHk3Fe484U83cG7c/PfCpYi5mfxia5R17tTw6', 1, NULL, '', 0, NULL),
+(1234, 'nube', 'sanchez', '$2y$10$LO2tg5C1Vt8fhsJOo2mRqeLjZG0iCkl.Gd80A0L9AZeGA719HaDDu', 5, 'nube9916@gmail.com', '', 0, NULL),
+(2025, 'patricia', 'patiño', '$2y$10$aiUAA6oEMZVf.YpYhBRwUOOBgbpFV0fm8f6o5.zQQiiqkDA/RcMzK', 3, NULL, '', 0, NULL),
+(4040, 'Super', 'Admin', '$2y$10$F22tgMXGlfLCFN2dRocsWOOlfWflEXeIbDcCJtfJXsEwMhnpRLtmS', 0, NULL, '', 0, NULL),
+(8080, 'Andres', 'Sanchez', '$2y$10$IabgsxxSgmKlBPoqBj4lmO8fI6OPGQK.DM70erEk9Bi8PxdJWXXzi', 5, 'vaca@gmail.com', '3104725242', 0, NULL);
 
 -- --------------------------------------------------------
 
@@ -126,7 +113,8 @@ CREATE TABLE `mensajes` (
 
 INSERT INTO `mensajes` (`id`, `nombre`, `email`, `mensaje`, `fecha`) VALUES
 (1, 'nube', 'usuario1@gmail.com', ',nmklnklklkljkl', '2025-04-01 20:54:22'),
-(2, 'Brian', 'brian@gmail.com', 'hola mi bro', '2025-04-01 21:10:17');
+(2, 'Brian', 'brian@gmail.com', 'hola mi bro', '2025-04-01 21:10:17'),
+(3, 'Juan', 'hola@gmail.com', 'llamame', '2025-04-02 23:14:30');
 
 -- --------------------------------------------------------
 
@@ -261,19 +249,9 @@ INSERT INTO `servicios_realizados` (`Servicio_Realizado_id`, `Cedula_Empleado_id
 (42, NULL, 'ABC123', 3, '2024-11-03', 'Avenida 60, Ciudad N', 'Cambio de aceite', 'aceite_abc123.jpg', 'Cambio de aceite y revisión de motor', 'Sin custodia', 1),
 (45, NULL, 'ABC123', 2, '2024-11-22', 'Poli JIC', 'Motor con sobrecalentamiento', 'carro.img', 'Reparación del sistema de enfriamiento', 'Custodia en taller hasta reparación', 1),
 (46, 1040, 'ABC123', 5, '2025-04-01', 'poli jic', 'camara', 'foto.img', 'revision', 'linterna', 1),
-(47, NULL, 'IEX747', 4, '2025-04-02', 'poli jic', NULL, NULL, 'quiero que vayan', '3102588225', 0);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `sincronizacion`
---
-
-CREATE TABLE `sincronizacion` (
-  `Sincronizacion_id` int(11) NOT NULL,
-  `Servicio_Realizado_id_Sincronizacion` int(11) DEFAULT NULL,
-  `Fecha_Sincronizacion` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+(47, NULL, 'IEX747', 4, '2025-04-02', 'poli jic', NULL, NULL, 'quiero que vayan', '3102588225', 0),
+(48, NULL, 'MNI982', 3, '2025-04-03', 'poli jic', NULL, NULL, 'Llevar la pinta de aceite', '3102588225', 0),
+(49, NULL, 'MNI982', 1, '2025-04-03', 'poli jic', NULL, NULL, 'cambio aceite', '3102588225', 0);
 
 -- --------------------------------------------------------
 
@@ -300,6 +278,7 @@ INSERT INTO `vehiculos` (`Placa`, `Marca`, `Modelo`, `Color`, `Objetos_Valiosos`
 ('GHI101', 'Honda', 'Civic', 'Negro', 'Aire acondicionado', 33333),
 ('IEX747', 'Duster Dynamiq', '2015', 'Blanco', 'celular', 5050),
 ('JKL112', 'Ford', 'Fiesta', 'Azul', 'Documentos', 44444),
+('MNI982', 'Renault', 'Logan', 'Negro', 'Computador', 5050),
 ('MNO345', 'Chevrolet', 'Spark', 'Verde', 'Teléfono', 11111),
 ('PQR678', 'Nissan', 'Sentra', 'Plateado', 'Bocinas', 33333),
 ('STU901', 'Kia', 'Rio', 'Amarillo', 'Cámara', 22222),
@@ -328,13 +307,6 @@ ALTER TABLE `clientes`
 --
 ALTER TABLE `empleados`
   ADD PRIMARY KEY (`Cedula_Empleado_id`);
-
---
--- Indices de la tabla `geolocalización`
---
-ALTER TABLE `geolocalización`
-  ADD PRIMARY KEY (`Geolocalización_id`),
-  ADD KEY `Servicios_realizados_id_Geolocalización` (`Servicios_realizados_id_Geolocalización`);
 
 --
 -- Indices de la tabla `mensajes`
@@ -379,13 +351,6 @@ ALTER TABLE `servicios_realizados`
   ADD KEY `Servicio_id_Servicios_Realizados` (`Servicio_id_Servicios_Realizados`);
 
 --
--- Indices de la tabla `sincronizacion`
---
-ALTER TABLE `sincronizacion`
-  ADD PRIMARY KEY (`Sincronizacion_id`),
-  ADD KEY `Servicio_Realizado_id_Sincronizacion` (`Servicio_Realizado_id_Sincronizacion`);
-
---
 -- Indices de la tabla `vehiculos`
 --
 ALTER TABLE `vehiculos`
@@ -403,16 +368,10 @@ ALTER TABLE `alertas_recordatorios`
   MODIFY `Alertas_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `geolocalización`
---
-ALTER TABLE `geolocalización`
-  MODIFY `Geolocalización_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT de la tabla `mensajes`
 --
 ALTER TABLE `mensajes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `permisos`
@@ -436,13 +395,7 @@ ALTER TABLE `servicios`
 -- AUTO_INCREMENT de la tabla `servicios_realizados`
 --
 ALTER TABLE `servicios_realizados`
-  MODIFY `Servicio_Realizado_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
-
---
--- AUTO_INCREMENT de la tabla `sincronizacion`
---
-ALTER TABLE `sincronizacion`
-  MODIFY `Sincronizacion_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Servicio_Realizado_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
 
 --
 -- Restricciones para tablas volcadas
@@ -453,12 +406,6 @@ ALTER TABLE `sincronizacion`
 --
 ALTER TABLE `alertas_recordatorios`
   ADD CONSTRAINT `alertas_recordatorios_ibfk_1` FOREIGN KEY (`Servicio_Realizado_id_alertas_recordatorios`) REFERENCES `servicios_realizados` (`Servicio_Realizado_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Filtros para la tabla `geolocalización`
---
-ALTER TABLE `geolocalización`
-  ADD CONSTRAINT `geolocalización_ibfk_1` FOREIGN KEY (`Servicios_realizados_id_Geolocalización`) REFERENCES `servicios_realizados` (`Servicio_Realizado_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `rol_permisos`
@@ -474,12 +421,6 @@ ALTER TABLE `servicios_realizados`
   ADD CONSTRAINT `servicios_realizados_ibfk_1` FOREIGN KEY (`Cedula_Empleado_id_Servicios_Realizados`) REFERENCES `empleados` (`Cedula_Empleado_id`) ON DELETE SET NULL ON UPDATE CASCADE,
   ADD CONSTRAINT `servicios_realizados_ibfk_2` FOREIGN KEY (`Vehiculo_id_Servicios_Realizados`) REFERENCES `vehiculos` (`Placa`) ON DELETE SET NULL ON UPDATE CASCADE,
   ADD CONSTRAINT `servicios_realizados_ibfk_3` FOREIGN KEY (`Servicio_id_Servicios_Realizados`) REFERENCES `servicios` (`Servicio_id`) ON DELETE SET NULL ON UPDATE CASCADE;
-
---
--- Filtros para la tabla `sincronizacion`
---
-ALTER TABLE `sincronizacion`
-  ADD CONSTRAINT `sincronizacion_ibfk_1` FOREIGN KEY (`Servicio_Realizado_id_Sincronizacion`) REFERENCES `servicios_realizados` (`Servicio_Realizado_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `vehiculos`
